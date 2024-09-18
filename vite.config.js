ead614d1dd483e7d05da14fbd141b364470afd04
muloo <fly20091124@126.com>
2024-09-18 14:33:52 +0800

    fix:remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://s.yupoo.com/redis-key/',
+  // base: 'https://s.yupoo.com/redis-key/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
